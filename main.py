6e051ff9b0b9e96b6fec03003c75625ecb47253b
= <candavis01@gmail.com>
2023-04-07 15:12:48 +0100

    test gui

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ from human_player import HumanPlayer
 from ai_player import AIPlayer
 import play
 
+import tkinter
+
+window = tkinter.Tk(className="Higher or Lower")
+window.mainloop()
+
 player = AIPlayer()
 play.multiple_game(player)
+
